49182d54612b7cd44aad595e3ccfed86dfc71281
Doug Headley <headley.douglas@gmail.com>
2013-10-16 15:57:10 -0700

    Omniauth made avaiable for user.   expects gplus, facebook and twitter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,7 +231,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :twitter, CONFIG[Rails.env.to_sym]["TWITTER"]["KEY"], CONFIG[Rails.env.to_sym]["TWITTER"]["SECRET"]
+  config.omniauth :gplus, CONFIG[Rails.env.to_sym]["GPLUS"]["KEY"], CONFIG[Rails.env.to_sym]["GPLUS"]["SECRET"]
+  config.omniauth :facebook, CONFIG[Rails.env.to_sym]["FACEBOOK"]["KEY"], CONFIG[Rails.env.to_sym]["FACEBOOK"]["SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
