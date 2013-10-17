c0777a03dda0902e4811238beb487e62de8356ab
Doug Headley <headley.douglas@gmail.com>
2013-10-16 17:40:21 -0700

    fixed bug in sytax.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user, notice: "Signed in!"
     else
       session["devise.user_attributes"] = user.attributes
-      redirect_to new_user_reqistration_url
+      redirect_to new_user_registration_url
     end
   end
 
