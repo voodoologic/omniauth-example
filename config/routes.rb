8ff8dfba6719790d6b368e8e7fb9cf51bb835a81
VoodooLogic <headley.douglas@gmail.com>
2013-10-19 00:30:17 -0700

    User routes for newly logged in users.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Omniauth::Application.routes.draw do
                     controllers: {omniauth_callbacks: "omniauth_callbacks"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
