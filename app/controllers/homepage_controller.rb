606a28ef509dfad6a121cbcf0611c560a8f6b17e
Doug Headley <headley.douglas@gmail.com>
2014-01-25 23:48:50 -0800

    Homepage with boilerplate.

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,6 @@
+class HomepageController < ApplicationController
+  skip_before_filter :authenticate_user!
+  def welcome
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Omniauth::Application.routes.draw do
   resources :users
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'homepage#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
