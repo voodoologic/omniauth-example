2397c1e1be9a1f090994bdcd5a2828f53143b4ba
Doug Headley <headley.douglas@gmail.com>
2014-02-15 13:10:54 -0800

    Logger awesome

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
-    raise request.env['omniauth.auth'].inspect
+    logger.debug request.env["omniauth.auth"]
     user = User.from_omniauth request.env["omniauth.auth"]
     if user.persisted?
       sign_in_and_redirect user, notice: "Signed in!"
