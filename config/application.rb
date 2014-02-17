6bb3ec0acbb8caeb68b9b79c86e515a2eebb033c
Doug Headley <headley.douglas@gmail.com>
2014-02-17 00:14:27 -0800

    Lib for twitter proceedures.

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -24,6 +25,7 @@ module Omniauth
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
