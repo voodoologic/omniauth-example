ebaccf8f87c294211ad53c50e0922ddbc9b27d55
Doug Headley <headley.douglas@gmail.com>
2014-02-17 19:33:55 -0800

    Added coordinates to tweet.

--- db/migrate/20140215194413_create_tweets.rb
+++ db/migrate/20140215194413_create_tweets.rb
@@ -4,11 +4,11 @@ class CreateTweets < ActiveRecord::Migration
       t.string :text
       t.integer :uid
       t.string :user_name
-      t.string :screen_name
       t.string :profile_image
       t.datetime :posted_at
-      t.string :coordinates
-      t.string :details
+      t.float :geo_lat
+      t.float :geo_lon
+      t.text :details
 
       t.timestamps
     end
